f4a2c5d15e17b14e8f8c87e6071783d7d0292a3e
Mani <manirajendran31jul@gmail.com>
2024-05-28 08:49:08 +0530

    Docker

---
+++ start-server.sh
@@ -0,0 +1,33 @@
+# Use the official Python image as base
+FROM python:3.8
+
+# Set environment variables
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+# Set working directory in the container
+WORKDIR /app
+
+# Install dependencies
+COPY requirements.txt /app/
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the Django project code into the container
+COPY . /app/
+
+# Collect static files
+RUN python manage.py collectstatic --noinput
+
+# Add a script to wait for the DB to be ready before starting the server
+COPY wait-for-it.sh /wait-for-it.sh
+RUN chmod +x /wait-for-it.sh
+
+# Add the start-server.sh script and make it executable
+COPY start-server.sh /app/start-server.sh
+RUN chmod +x /app/start-server.sh
+
+# Expose the port on which your Django app will run
+EXPOSE 8000
+
+# Command to run the Django development server
+CMD ["/app/start-server.sh"]
