a8ae210fc763605bb5405ab6ecf50c99753a8267
Mani <manirajendran31jul@gmail.com>
2024-05-28 08:53:24 +0530

    Docker

--- start-server.sh
+++ start-server.sh
@@ -1,33 +1,10 @@
-# Use the official Python image as base
-FROM python:3.8
+#!/bin/sh
 
-# Set environment variables
-ENV PYTHONDONTWRITEBYTECODE 1
-ENV PYTHONUNBUFFERED 1
+# Wait for PostgreSQL to be ready
+/wait-for-it.sh $DATABASE_HOST:$DATABASE_PORT --timeout=60 --strict -- echo "PostgreSQL is up - executing command"
 
-# Set working directory in the container
-WORKDIR /app
+# Apply database migrations
+python manage.py migrate
 
-# Install dependencies
-COPY requirements.txt /app/
-RUN pip install --no-cache-dir -r requirements.txt
-
-# Copy the Django project code into the container
-COPY . /app/
-
-# Collect static files
-RUN python manage.py collectstatic --noinput
-
-# Add a script to wait for the DB to be ready before starting the server
-COPY wait-for-it.sh /wait-for-it.sh
-RUN chmod +x /wait-for-it.sh
-
-# Add the start-server.sh script and make it executable
-COPY start-server.sh /app/start-server.sh
-RUN chmod +x /app/start-server.sh
-
-# Expose the port on which your Django app will run
-EXPOSE 8000
-
-# Command to run the Django development server
-CMD ["/app/start-server.sh"]
+# Start the Django server
+exec gunicorn Rentify.wsgi:application --bind 0.0.0.0:8000
