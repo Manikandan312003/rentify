3a6451bf711e9380e040bc7080b4a5f79467f754
Manikandan312003 <106978783+Manikandan312003@users.noreply.github.com>
2024-05-28 13:28:56 +0530

    change debug to false

--- Rentify/settings.py
+++ Rentify/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("ENVIRONMENT") == "development"
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = [
     "*"
